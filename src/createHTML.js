6b58c36195cf2635c80843478b8e0bd6b6f80213
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-11-30 15:39:30 -0500

    mutiple employee types

--- src/createHTML.js
+++ src/createHTML.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 // Creates HTML based on the team members entered
 
-function createHTML(team) {
+function createHTML(data) {
 
   return `
     <!DOCTYPE html>
@@ -38,7 +38,7 @@ function createHTML(team) {
       <!-- Creates the Overall Page -->
       <div class="container">
     
-        <!-- Row 1 -->
+        <!-- Header row -->
         <div class="row">
           <div class="col-sm-12" id="profileheader">
             <h1 class="text-center">Development Team</h1>
@@ -46,43 +46,9 @@ function createHTML(team) {
         </div>
       <!-- Only one manager and always first -->
         <div class="row card-columns">
-        <div class="card">
-          <div class="card-body" id="employeeheader">
-          <h4 class="card-title">${team[0].name}</h4>
-          <h5 class="card-text">Manager</h5>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">ID: ${team[0].id}</li>
-          <li class="list-group-item">Email: ${team[0].email}</li>
-          <li class="list-group-item">Office Number: ${team[0].officeNumber}</li>
-        </ul>
-        <!-- to do: create loops for engineers and interns -->
-        </div>
-        <div class="card">
-        <div class="card-body" id="employeeheader">
-        <h4 class="card-title">${team[1].name}</h4>
-        <h5 class="card-text">Engineer</h5>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${team[1].id}</li>
-        <li class="list-group-item">Email: ${team[1].email}</li>
-        <li class="list-group-item">GitHub: ${team[1].githubUsername}</li>
-      </ul>
-      </div>
-      </div>
-        <div class="card">
-        <div class="card-body" id="employeeheader">
-        <h4 class="card-title">${team[2].name}</h4>
-        <h5 class="card-text">Intern</h5>
-      </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">ID: ${team[2].id}</li>
-        <li class="list-group-item">Email: ${team[2].email}</li>
-        <li class="list-group-item">School: ${team[2].schoolname}</li>
-      </ul>
-      </div>
-       
-    
+        
+     ${createEmployeeCards(data)}
+     
     </body>
     
     </html>   
@@ -90,5 +56,70 @@ function createHTML(team) {
     `;
 }
 
+function createEmployeeCards(data) {
+  return data.map((data) => {
+    let role = data.getRole();
+    switch(role) {
+      case "Manager":
+        return createManagercard(data);
+        break;
+        case "Engineer":
+        return createEngineercard(data);
+        break;
+        case "Intern":
+        return createInterncard(data);
+        break;
+    }
+  })
+};
+
+function createManagercard(data) {
+  return `
+  <div class="card">
+          <div class="card-body" id="employeeheader">
+          <h4 class="card-title">${data.name}</h4>
+          <h5 class="card-text">Manager</h5>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${data.id}</li>
+          <li class="list-group-item">Email: ${data.email}</li>
+          <li class="list-group-item">Office Number: ${data.officeNumber}</li>
+        </ul> 
+       </div> 
+      </div>
+      `
+};
+
+function createEngineercard(data) {
+  return `
+  <div class="card">
+  <div class="card-body" id="employeeheader">
+  <h4 class="card-title">${data.name}</h4>
+  <h5 class="card-text">Engineer</h5>
+</div>
+<ul class="list-group list-group-flush">
+  <li class="list-group-item">ID: ${data.id}</li>
+  <li class="list-group-item">Email: ${data.email}</li>
+  <li class="list-group-item">GitHub: ${data.githubUsername}</li>
+</ul>
+</div>
+`
+};
+
+function createInterncard(data) {
+  return `
+<div class="card">
+<div class="card-body" id="employeeheader">
+<h4 class="card-title">${data.name}</h4>
+<h5 class="card-text">Intern</h5>
+</div>
+<ul class="list-group list-group-flush">
+<li class="list-group-item">ID: ${data.id}</li>
+<li class="list-group-item">Email: ${data.email}</li>
+<li class="list-group-item">School: ${data.schoolname}</li>
+</ul>
+</div>
+`
+};
 // exports the createHTML function
 module.exports = createHTML;
